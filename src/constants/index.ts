021619ae9bc64f7d60269740d3876d0ae2d3dd45
Timo Engler <timo.engler1@web.de>
2025-11-02 20:49:13 -0500

    Add constants file

---
+++ src/constants/index.ts
@@ -0,0 +1,10 @@
+// App constants
+
+export const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL || '';
+
+// Storage keys
+export const STORAGE_KEYS = {
+  AUTH_TOKEN: 'auth_token',
+  USER_DATA: 'user_data',
+} as const;
+
